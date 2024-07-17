5e13b6889364ab547721b0039b8f1b74c07f9ef5
czubaker <czubaker@gmail.com>
2024-07-17 12:01:18 +0200

    czubaker commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
 import Image from "next/image";
 import Header from "../Components/Header";
 import Head from "../Components/Head";
-import Carousel from "../Components/Carousel";
+// import Carousel from "../Components/Carousel";
 import Info from "../Components/Info";
 export default function Home() {
   return (
     <>
       <Head />
       <Header />
-      <Carousel />
-      <h2 className="text-center text-4xl mt-10 mb-10">ПОЧЕМУ ВЫБИРАЮТ НННАС</h2>
+      {/* <Carousel /> */}
+      <h2 className="text-center text-4xl mt-10 mb-10">ПОЧЕМУ ВЫБИРАЮТ НАС</h2>
       <Info />
     </>
   );
